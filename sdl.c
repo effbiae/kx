d4064a97f80818c4a34ddfc8873da560b50ccca7
jack andrews <effbiae@ivorykite.com>
2016-08-06 17:55:43 +1000

    got text drawing!  (w,h not bottom/right) simplified error checking for lib funcs

--- sdl.c
+++ sdl.c
@@ -1,5 +1,4 @@
-//install libcairo2-dev libsdl2-dev libsdl2-ttf-dev
-#include<SDL2/SDL.h>
+//install libsdl2-dev libsdl2-ttf-dev [libcairo2-dev]
 #include<SDL2/SDL_ttf.h>
 #include"k.h"
 #define A(x) if(!(x))O("A(%s)@%d\n",#x,__LINE__),exit(*(S)0);  //assert - simplistic error handling
@@ -7,16 +6,14 @@
 #define ON(x,y,z) IN(x,y,z+1)
 
 #define crs(c) cairo_status_to_string(cairo_status((V*)c))
-#define xsx(T,r,f,a,c,z,e) if(!((r=f a) c z)){fprintf(stderr,"*%s:%s\n",#f,e),fflush(stderr),exit(*(S)0);}
-#define Asx(T,r,f,a,c,z)   xsx(T,r,SDL_##f,a,c,z,SDL_GetError())
-#define Acx(T,r,f,a,c,z)   xsx(T,r,cairo_##f,a,c,z,crs(r))
-#define Atx(T,r,f,a,c,z)   xsx(T,r,TTF_##f,a,c,z,TTF_GetError())
-#define SA(f,a) ({I _r;Asx(I,_r,f,a,==,0);})
-#define SP(r,f,a) Asx(V*,r,f,a,!=,(V*)0)
-#define TA(f,a) ({I _r;Atx(I,_r,f,a,==,0);})
-#define TP(r,f,a) Atx(V*,r,f,a,!=,(V*)0)
-#define CA(f,a) ({I _r;Acx(I,_r,f,a,==,0);})
-#define CP(r,f,a) Acx(V*,r,f,a,!=,(V*)0)
+S SDL_e(V*x){R (S)SDL_GetError();}S TTF_e(V*x){R (S)TTF_GetError();}/*S Cer(V*x){R (S)cairo_status_to_string(cairo_status(x));}*/
+#define xsx(c,r,f,a,e) if(c(r=f a)){O("*%s:%s %d\n",#f,e,__LINE__),exit(*(S)0);}
+#define XY(x,c,r,f,a) xsx(c,r,x##_##f,a,x##_e((V*)r))
+#define XZ(x,c,f,a) ({J _r;XY(x,c,_r,f,a);})
+#define SA(f,a)   XZ(SDL,!!, f,a)
+#define SP(r,f,a) XY(SDL,!,r,f,a)
+#define TA(f,a)   XZ(TTF,!!, f,a)
+#define TP(r,f,a) XY(TTF,!,r,f,a)
 
 struct {SDL_Renderer*v;SDL_Window*w;TTF_Font*f;}g;
 Z K1(draw)
@@ -27,11 +24,11 @@ Z K1(draw)
  R kj(9);
 }
 Z K1(letter)
-{TTF_Font*f=TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 24);
+{TTF_Font*f=TTF_OpenFont("DejaVuSansMono.ttf", 12);
  S s="hello";I d[2];TA(SizeText,(f,s,d,d+1));
  SDL_Color b={0,0,0};SDL_Surface*u;TP(u,RenderText_Shaded,(f,s,b,(SDL_Color){255,255,255}));
  SDL_Texture*a;SP(a,CreateTextureFromSurface,(g.v,u));
- SDL_Rect e={100,100,d[0],d[1]};
+ SDL_Rect e={0,0,d[0],d[1]};
  SA(RenderCopy,(g.v,a,0,&e));
  SDL_RenderPresent(g.v);
  SDL_DestroyTexture(a);
