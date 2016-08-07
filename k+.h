a62cd828e35aee1d1ede3db3e1dcef3be1af0369
jack andrews <effbiae@ivorykite.com>
2016-08-07 21:23:22 +1000

    added k+.h - yj, zj, rj...

---
+++ k+.h
@@ -0,0 +1,92 @@
+#include"k.h"
+/*
+// remove more clutter
+#define O printf
+#define R return
+#define Z static
+#define P(x,y) {if(x)R(y);}
+#define U(x) P(!(x),0)
+#define SW switch
+#define CS(n,x)	case n:x;break;
+#define CD default
+*/
+#define xc TX(C,x)
+
+#define yr y->r
+#define yt y->t
+#define yu y->u
+#define yn y->n
+#define yy yK[0]
+#define yy yK[1]
+#define yg TX(G,y)
+#define yc TX(C,y)
+#define yh TX(H,y)
+#define yi TX(I,y)
+#define yj TX(J,y)
+#define ye TX(E,y)
+#define yf TX(F,y)
+#define ys TX(S,y)
+#define yk TX(K,y)
+#define yG y->G0
+#define yH ((H*)yG)
+#define yI ((I*)yG)
+#define yJ ((J*)yG)
+#define yE ((E*)yG)
+#define yF ((F*)yG)
+#define yS ((S*)yG)
+#define yK ((K*)yG)
+#define yC yG
+#define yB ((G*)yG)
+
+#define zr z->r
+#define zt z->t
+#define zu z->u
+#define zn z->n
+#define zz zK[0]
+#define zz zK[1]
+#define zg TX(G,z)
+#define zc TX(C,z)
+#define zh TX(H,z)
+#define zi TX(I,z)
+#define zj TX(J,z)
+#define ze TX(E,z)
+#define zf TX(F,z)
+#define zs TX(S,z)
+#define zk TX(K,z)
+#define zG z->G0
+#define zH ((H*)zG)
+#define zI ((I*)zG)
+#define zJ ((J*)zG)
+#define zE ((E*)zG)
+#define zF ((F*)zG)
+#define zS ((S*)zG)
+#define zK ((K*)zG)
+#define zC zG
+#define zB ((G*)zG)
+
+#define rr r->r
+#define rt r->t
+#define ru r->u
+#define rn r->n
+#define rr rK[0]
+#define rr rK[1]
+#define rg TX(G,r)
+#define rc TX(C,r)
+#define rh TX(H,r)
+#define ri TX(I,r)
+#define rj TX(J,r)
+#define re TX(E,r)
+#define rf TX(F,r)
+#define rs TX(S,r)
+#define rk TX(K,r)
+#define rG r->G0
+#define rH ((H*)rG)
+#define rI ((I*)rG)
+#define rJ ((J*)rG)
+#define rE ((E*)rG)
+#define rF ((F*)rG)
+#define rS ((S*)rG)
+#define rK ((K*)rG)
+#define rC rG
+#define rB ((G*)rG)
+
