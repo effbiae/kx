51a4e6f9a34e227a08a0317152a0b3c8c018ab01
jack andrews <effbiae@ivorykite.com>
2016-08-11 12:54:33 +1000

    tidy

--- g.c
+++ g.c
@@ -91,9 +91,7 @@ Z K1(call)
 Z K1(font){TP(g.f,OpenFont,("DejaVuSansMono.ttf",14));I d[2];TA(SizeText,(g.f,"Wy",d,d+1));*d=*d/2;DO(2,g.d[i]=d[i]);R kj(7);}
 ZI g0(){SDL_Init(SDL_INIT_EVERYTHING);TTF_Init();font(0);}
 ZI sel(I c,F t){I r;fd_set f,*p=&f;FD_ZERO(p);if(-1<c)FD_SET(c,p);else p=0;long s=t,v[]={s,1e6*(t-s)};AS(r=select(c+1,p,(V*)0,(V*)0,(V*)v));P(r&&FD_ISSET(c,&f),c)R 0;}
-ZK sr(I c){I t;K x;
- A(x=k(c,(S)0));
- R k(-c,"",call(x),(K)0);} //async from q
+ZK sr(I c){I t;K x;A(x=k(c,(S)0));R k(-c,"",call(x),(K)0);} //async from q
 ZI fexec(I n,S*v)
 {I p=5001;I kon(F t,I n){N(n,P(0<(g.c=khp("localhost",p)),g.c)sel(g.c,t));R g.c;}I wat(J p){I s;R A(waitpid(g.q,&s,0)),A(WIFEXITED(s)),0;}
  AS(g.q=fork());$(!g.q,AS(execvp("q",(S[]){"q","g.k",0}))){kon(1e-1,10);P(g.c,g.c)wat(g.q);}}
